0e81c926f4f3b7fcbef310059a3920d8030ccad4
Elybourland <JEBourland@yahoo.com>
2023-10-06 17:41:09 -0700

    creating pages

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eligendi non ipsam iste ullam necessitatibus nulla eos. Suscipit, est a!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eligendi non ipsam iste ullam necessitatibus nulla eos. Suscipit, est a!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
