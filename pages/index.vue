c0ae1a15e9ff8ab8c0b5adb48d4a4ea1bd62c154
Elybourland <JEBourland@yahoo.com>
2023-10-07 13:29:55 -0700

    nuxtlink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
     <h2>Home</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eligendi non ipsam iste ullam necessitatibus nulla eos. Suscipit, est a!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eligendi non ipsam iste ullam necessitatibus nulla eos. Suscipit, est a!</p>
