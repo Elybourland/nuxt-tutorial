3ccc8f3fa48e3b3e21c78801ba334f23bce2e484
Elybourland <JEBourland@yahoo.com>
2023-10-06 17:49:43 -0700

    route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae eaque excepturi totam inventore, ratione est dolor ullam atque voluptatum!</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
